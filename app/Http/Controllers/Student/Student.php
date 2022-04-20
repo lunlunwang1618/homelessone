42760343f8346f56a2efa84654d2f6066ba715f4
DNIB <qazs0205@gmail.com>
2022-04-21 01:23:08 +0800

    Update Student

--- app/Http/Controllers/Student/Student.php
+++ app/Http/Controllers/Student/Student.php
@@ -6,6 +6,20 @@ use App\Http\Controllers\Controller;
 
 class Student extends Controller
 {
+    public function __construct(
+        string $name,
+        int $age,
+        string $sex,
+        string $phone,
+        string $fruit
+    ) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->sex = $sex;
+        $this->phone = $phone;
+        $this->fruit = $fruit;
+    }
+
     public string $name;
     public int $age;
     public string $sex;

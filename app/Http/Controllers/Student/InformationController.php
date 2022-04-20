dc98a4a67ecc52626e2d9f8c3280107a8d22f7d7
DNIB <qazs0205@gmail.com>
2022-04-21 01:12:40 +0800

    Todo: Render student-information-page

---
+++ app/Http/Controllers/Student/InformationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+
+class InformationController extends Controller
+{
+    public function index(): View
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Student/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+
+class Student extends Controller
+{
+    public string $name;
+    public int $age;
+    public string $sex;
+    public string $phone;
+    public string $fruit;
+}

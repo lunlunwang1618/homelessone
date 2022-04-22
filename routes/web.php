a7c0d52b2c0b16df2a285cc0cfa549fdb3b9d2e6
lunlunwang1618 <lunlunwang1618@gmail.com>
2022-04-22 23:11:41 +0800

    error

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', [SampleController::class ,'menberlist']);
+Route::get('/test', [SampleController::class , 'menberlist']);
